af1c8fcf605378dd7ef49ea2246ff9f1a555e665
Zeuxis Lo <seekstudio@gmail.com>
2014-04-28 14:05:46 +0800

    complete test_decode_image test case

--- tests/test_qqsuperman.py
+++ tests/test_qqsuperman.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import pytest
 import unittest
 from collections import namedtuple
@@ -20,7 +21,10 @@ class TestQQSuperman(unittest.TestCase):
         self.assertRegexpMatches(self.superman.remainder_point(), r'^[0-9]+$')
 
     def test_decode_image(self):
-        pass
+        content = self.superman.decode_image(os.path.dirname(__file__) + '/sample/1.jpg')
+
+        self.assertEqual(content['code'].lower(), 'nmvt')
+        self.assertEqual(len(content['worker']), 32)
 
     def test_report_error(self):
         pass
